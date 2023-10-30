81ae093e317ded45735b44aa3e8f0f3c721d59a8
Lalitho <lalithohb@gmail.com>
2023-10-29 23:41:42 -0600

    v0.2

--- src/RecibirInformacion.java
+++ src/RecibirInformacion.java
@@ -1,31 +1,57 @@
+import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.InputStreamReader;
 import java.net.Socket;
 
 public class RecibirInformacion extends Thread {
-  Socket s;
+  private Socket cliente;
+  private DataOutputStream enviar;
+  private DataInputStream leer;
 
-  public RecibirInformacion(Socket s) {
-    this.s = s;
-  }
-
-  public void recibirInformacion() {
+  public RecibirInformacion(Socket cliente) {
+    this.cliente = cliente;
     try {
-      while (true) {
-        DataInputStream recibir = new DataInputStream(s.getInputStream());
-        DataOutputStream enviar = new DataOutputStream(s.getOutputStream());
-        String mensaje;
-        mensaje = recibir.readUTF();
-        System.out.println(mensaje);
-        enviar.writeUTF(mensaje);
-      }
+      this.enviar = new DataOutputStream(cliente.getOutputStream());
+      this.leer = new DataInputStream(cliente.getInputStream());
     } catch (Exception e) {
       System.err.println(e.getMessage());
     }
   }
 
+  private void manejarChat(String mensaje) {
+//    String mensaje;
+    for (Socket c : Servidor.clientes) {
+      if (c != this.cliente) {
+        try {
+          DataOutputStream enviar_ = new DataOutputStream(c.getOutputStream());
+//          DataInputStream leer_ = new DataInputStream(c.getInputStream());
+//          mensaje = leer_.readUTF();
+//          System.out.println(mensaje);
+          enviar_.writeUTF(mensaje);
+        } catch (Exception e) {
+          System.out.println(e.getMessage());
+        }
+      }
+    }
+  }
+
   @Override
   public void run() {
-    recibirInformacion();
+    try {
+      String mensaje;
+      System.out.println("Se unio al server");
+      while (true) {
+        BufferedReader leer = new BufferedReader(new InputStreamReader(System.in));
+        mensaje = leer.readLine();
+        manejarChat(mensaje);
+      }
+//      Servidor.clientes.remove(this.cliente);
+//      cliente.close();
+//      System.out.println("Se desconecto");
+    } catch (Exception e) {
+      System.err.println(e.getMessage());
+    }
   }
+
 }
--- src/Servidor.java
+++ src/Servidor.java
@@ -5,64 +5,28 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 
-public class Servidor extends Thread {
-  ServerSocket servidor;
-  Socket s;
-  ArrayList<Servidor> sockets;
-  ArrayList<String> usuarios;
+public class Servidor {
+  private static ServerSocket servidor;
+  static ArrayList<Socket> clientes;
 
-  Servidor(ServerSocket servidor, Socket s) {
-    this.servidor = servidor;
-    this.s = s;
-    this.usuarios = new ArrayList<String>();
-    this.sockets=new ArrayList<Servidor>();
-  }
-
-  Servidor() {
-    try {
-      servidor = new ServerSocket(9999);
-      this.usuarios = new ArrayList<String>();
-    } catch (Exception e) {
+  public Servidor(){
+    try{
+      servidor=new ServerSocket(9999);
+      clientes = new ArrayList<>();
+    } catch (Exception e){
       System.err.println(e.getMessage());
     }
   }
 
-  public void IniciarServidor() {
-    System.out.println("Servidor iniciado...");
-    try {
-      while (true) {
-        s = servidor.accept();
-        Servidor usr = new Servidor(servidor, s);
-        sockets.add(usr);
+  public void IniciarServidor(){
+    try{
+      System.out.println("Servidor Iniciado");
+      while (true){
+        Socket cliente = servidor.accept();
+        clientes.add(cliente);
+        RecibirInformacion usr = new RecibirInformacion(cliente);
         usr.start();
       }
-    } catch (Exception e) {
-      System.err.println(e.getMessage());
-    }
-  }
-
-  @Override
-  public void run() {
-//    EnviarInformacion env = new EnviarInformacion(s);
-    RecibirInformacion recv = new RecibirInformacion(s);
-    try {
-      //env.start();
-      regUsuario();
-      recv.start();
-    } catch (Exception e) {
-      System.err.println(e.getMessage());
-    }
-  }
-  public void regUsuario(){
-    try{
-      DataInputStream recibir = new DataInputStream(s.getInputStream());
-      DataOutputStream enviar = new DataOutputStream(s.getOutputStream());
-      String mensaje;
-      System.out.println("Llego nuevo usuario.");
-      mensaje = recibir.readUTF();
-      usuarios.add(mensaje);
-      //Agregar a Archivo de datos
-      enviar.writeUTF(mensaje + " registrado con exito.");
     }catch (Exception e){
       System.err.println(e.getMessage());
     }
